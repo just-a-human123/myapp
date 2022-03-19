f55c97b9cab48fa69e6b7d7665dc539265ae48ef
just-a-human123 <83978538+just-a-human123@users.noreply.github.com>
2022-03-19 21:33:36 +0700

    add some image and import axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-import { useEffect } from 'react';
+import axios from 'axios';
+import react from 'react';
 
 
 const SPOTIFY_CLIENT_ID = process.env.REACT_APP_SPOTIFY;
 
-export default ;
+function App() {
+        return (
+                <div className="App">
+                 <header className="App-header">
+
+                   <img src="https://img.freepik.com/free-vector/paw-print-stic>
+                   <img src="" className="" alt=""/>
+                   <p></p>
+                   <a>Something?</a>
+                 </header>
+                </div>
+        );
+}
+
+
+
+export default App;
+
+
