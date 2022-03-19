eaebf6ae8a4334061eca41139b14075350f38311
alfazari <faa04807@gmail.com>
2022-03-19 16:37:05 +0700

    mencoba membuat sebuah tampilan dengan function app

--- src/App.js
+++ src/App.js
@@ -3,4 +3,19 @@ import { useEffect } from 'react';
 
 const SPOTIFY_CLIENT_ID = process.env.REACT_APP_SPOTIFY;
 
-export default ;
+
+function App() {
+	return (
+		<div className="App">
+		 <header className="App-header">
+		   <img src="" className="" alt=""/>
+		   <p></p>
+		   <a>Something?</a>
+		 </header>
+		</div>
+	);
+}
+
+
+
+export default App;
